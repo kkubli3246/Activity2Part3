9dc03e382807000eaf5a0325bba2b0761bfa2aa4
Kekoa Kubli <kkubli3246@gmail.com>
2020-07-25 22:24:03 -0700

    COmplete Activity PArt3

---
+++ Activity1part3/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Activity1part3.Models;
+using Activity1part3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1part3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel user) 
+        {
+            SecurityService service = new SecurityService();
+
+            bool check = service.Authenticate(user);
+
+            if (check)
+            {
+                return View("LoginPassed");
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+        }
+    }
+}
